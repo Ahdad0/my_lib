f233d1eccaef68c017705e81f884d914e4829bda
Abderrahim Ahaded <abahaded@e2r3p11.1337.ma>
2024-10-29 17:25:38 +0100

    d

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,13 @@
 /*   By: abahaded <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 16:25:37 by abahaded          #+#    #+#             */
-/*   Updated: 2024/10/29 11:25:30 by abahaded         ###   ########.fr       */
+/*   Updated: 2024/10/29 13:54:17 by abahaded         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
@@ -20,16 +22,22 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	l = ft_strlen(little);
 	i = 0;
-	if (!little && len >= 0)
+	if (*little == '\0')
 		return ((char  *)big);
 	while (big[i])
 	{
 		j = 0;
 		while (big[i + j] == little[j] && little[j] && big[i + j])
 			j++;
-		if (j >= l && i + j < len)
+		if (j >= l && i + j <= len)
 				return ((char *)big + i);
 		i++;
 	}
 	return (NULL);
 }
+/*#include <stdio.h>
+int main()
+{
+	char *p = ft_strnstr("abcdefgh", "abc", 3);
+	printf("%s\n" ,p);
+}*/
