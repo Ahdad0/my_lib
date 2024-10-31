6281ed0ab0c0f69f1ebb2be9138541bfc2c38fb6
Abderrahim Ahaded <abahaded@e2r3p11.1337.ma>
2024-10-31 10:49:41 +0100

    d

--- ft_split.c
+++ ft_split.c
@@ -5,12 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abahaded <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/25 16:18:09 by abahaded          #+#    #+#             */
-/*   Updated: 2024/10/30 12:39:20 by abahaded         ###   ########.fr       */
+/*   Created: 2024/10/31 10:25:08 by abahaded          #+#    #+#             */
+/*   Updated: 2024/10/31 10:49:00 by abahaded         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 static int	count(char const *s, char c)
 {
@@ -21,7 +22,7 @@ static int	count(char const *s, char c)
 	cou = 0;
 	while (s[i])
 	{
-		if ((s[i] != c && s[i - 1] == c) || (s[i] != c && s[i + 1] == '\0'))
+		if ((s[i] != c && s[i + 1] == c) || (s[i] != c && s[i + 1] == '\0'))
 			cou++;
 		i++;
 	}
@@ -40,56 +41,68 @@ static void	free_p(char **s, unsigned int o)
 	free(s);
 }
 
+static int	ft_l(char *s, char c, int *j)
+{
+	int	len;
+
+	len = 0;
+	while (s[*j])
+	{
+		if (s[*j] != c)
+			len++;
+		else if (len != 0 && s[*j] == c)
+			break ;
+		(*j)++;
+	}
+	return (len);
+}
+
+static char	*ft_str(char *s, char c, int *j)
+{
+	char	*p;
+	int		i;
+	int		o;
+	int		len;
+
+	o = 0;
+	i = *j;
+	while (s[i] == c)
+		i++;
+	p = malloc(ft_l(s, c, j) + 1);
+	if (!p)
+		return (NULL);
+	len = *j;
+	while (i < len)
+		p[o++] = s[i++];
+	p[o] = '\0';
+	return (p);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	int		i;
-	int		o;
-	int		start;
+	int		num;
+	int		j;
 	char	**n;
 
 	i = 0;
-	o = 0;
-	start = 0;
-	n = malloc((count(s, c) + 1) * (sizeof(char *)));
+	j = 0;
+	num = count(s, c);
+	n = malloc((num + 1) * (sizeof(char *)));
 	if (!n)
 	{
 		free(n);
 		return (NULL);
 	}
-	while (s[i])
+	while (num--)
 	{
-		if (s[i] == c)
+		n[i++] = ft_str((char *)s, c, &j);
+		if (!n)
 		{
-			n[o++] = ft_substr(s, start, i - start);
-			if (!n)
-			{
-				free_p(n, o);
-				return (NULL);
-			}
-			start = i + 1;
+			free_p(n, c);
+			return (NULL);
 		}
-		i++;
 	}
-	n[o] = ft_substr(s, start, i - start);
-	if (!n)
-	{
-		free_p(n, o);
-		return (NULL);
-	}
+	n[i] = (char *) '\0';
 	return (n);
-}
-int main()
-{
-	char **s = ft_split("   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ');
-	int i = 0, j = 0;
-	while (s[i])
-	{
-		while (s[j])
-		{
-			printf("%c", s[i][j]);
-			j++;
-		}
-		printf("\n");
-		i++;
-	}
 }
